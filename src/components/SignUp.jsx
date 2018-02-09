218817d961bcb947b760ce5de5f6471480fad2e4
mp <michael.pinaud@gmail.com>
2018-02-09 15:35:44 -0800

    create and stylize sign up input and button on sign up component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,7 +3,42 @@ import React from 'react';
 function SignUp() {
   return (
     <div>
-      Sign Up!
+      <style jsx>{`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+        font-family: 'Roboto Condensed', sans-serif;
+        color: #BA5624;
+        h2 {
+          margin: 90px 0 20px;
+          font-size: 33px;
+        }
+        input {
+          margin-bottom: 20px;
+          border: 3px solid #617073;
+          background: none;
+          width: 200px;
+          height: 28px;
+        }
+        button {
+          background: none;
+          border: 3px solid #617073;
+          color: #BA5624;
+          font-family: 'Roboto Condensed', sans-serif;
+          height: 32px;
+          font-size: 15px;
+        }
+      `}</style>
+      <h2>Sign Up</h2>
+      <label for="user-email">Email</label>
+      <input type="email"></input>
+      <label for="user-password">Password:</label>
+      <input type="password"></input>
+      <label for="user-name">Name:</label>
+      <input type="name"></input>
+      <button>Sign Up</button>
     </div>
   );
 }
