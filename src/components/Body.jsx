4cfadf5b5bfb709dc81407aaef1f80b4b0107953
mp <michael.pinaud@gmail.com>
2018-02-09 13:35:19 -0800

    stylize home page including logo and router links

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,9 @@ import Home from './Home';
 function Body() {
   return (
     <div>
+      <style jsx>{`
+        margin-top: 10px;
+      `}</style>
       <Home />
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 function Home() {
+  const link = {
+    textDecoration:'none',
+    color: '#BA5624',
+  };
+
   return (
     <div>
-      <h1>Band-Aid</h1>
-      <p>Band-Aid is a record collection and wishlist inventory management system. <Link to='/SignUp'>Join us</Link> or <Link to='/Login'>Login</Link> start managing your collection now!</p>
+      <style jsx>{`
+        margin-top: 50px;
+        text-align: center;
+        font-family: 'Roboto Condensed', sans-serif;
+        h1 {
+          font-family: 'Megrim', cursive;
+          font-size: 10vh;
+          margin-bottom: 40px;
+          color: #BA5624;
+          font-weight: bolder;
+        }
+      `}</style>
+      <h1>band-aid</h1>
+      <p>Band-Aid is a record collection and wishlist inventory management system. <Link style={link} to='/SignUp'>Join us</Link> or <Link style={link} to='/Login'>Login</Link> start managing your collection now!</p>
     </div>
   );
 }
