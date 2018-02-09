ed60f0b78d1a9d5503679aa7b5100b9c9027d60c
mp <michael.pinaud@gmail.com>
2018-02-09 13:47:50 -0800

    update screenshot.png to add UserProfileList and UserProfile to the tree

--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 function Profiles() {
   return (
     <div>
-      Profiles
+      <style jsx>{`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Roboto Condensed', sans-serif;
+        h2 {
+          font-size: 2rem;
+          margin-top: 50px;
+          color: #BA5624;
+          margin-bottom: 20px;
+        }
+      `}</style>
+      <h2>Users</h2>
     </div>
   );
 }
